72d72822391debee865d9c3f2e1e35e31550154e
Khalil <khaleal@hotmail.com>
2021-01-22 12:52:54 +0200

    users object

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const app  = express();
+const app = express();
+
 
 app.use(express.static('public'));
+
+const users = [
+  {name:'Kilani', id:'222222222'},
+  {name:'Yara', id:'333333333'},
+  {name:'Ihab', id:'444444'}
+]
+
+//route
+app.get('/get-users',(req, res)=>{
+  res.send(users)
+})
+
+
+
 
 const port = process.env.PORT || 3000;
 
